070e50c7610c3280240971fdfffa6802776bf2fd
taulfsime <kris.td@icloud.com>
2025-09-10 18:50:28 +0300

    move db creds to dev env file and add basic example how to connect to db

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-console.log('test');
+import dotenv from 'dotenv';
+import { Client } from 'pg';
 
-console.log('test');
+dotenv.config();
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
+
+async function main() {
+  await client.connect();
+  console.log('Connected to the database!');
+
+  const res = await client.query('SELECT * FROM "chef";');
+  console.log('Chefs:', res.rows);
+
+  await client.end();
+}
+
+main().catch(err => console.error('Error:', err));
