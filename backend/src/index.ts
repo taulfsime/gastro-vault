b9681eea1a02d69406f2692d874dd1dffeb40d49
taulfsime <kris.td@icloud.com>
2025-09-13 17:24:46 +0300

    add express app to list chefs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
 import { Client } from 'pg';
+import express from 'express';
 
-dotenv.config();
+dotenv.config({
+  path: '../.env',
+});
+
+const app = express();
 
 const client = new Client({
   user: process.env.DB_USER,
@@ -11,14 +16,25 @@ const client = new Client({
   port: Number(process.env.DB_PORT),
 });
 
-async function main() {
-  await client.connect();
-  console.log('Connected to the database!');
+app.get('/chefs', async (req, res) => {
+  try {
+    await client.connect();
+    const result = await client.query('SELECT * FROM "chef";');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error fetching chefs:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    await client.end();
+  }
+});
 
-  const res = await client.query('SELECT * FROM "chef";');
-  console.log('Chefs:', res.rows);
+const server = app.listen(process.env.BE_PORT, () => {
+  console.log(`Backend is running on port ${process.env.BE_PORT}`);
+});
 
-  await client.end();
+function shutdown() {
+  server.close(() => process.exit(0));
 }
-
-main().catch(err => console.error('Error:', err));
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
